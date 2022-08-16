176ed3f0aac4ccfb2e268ab9cb751de0aea5502c
Abhishek <abhishekmalhotra1292@gmail.com>
2022-08-16 22:20:09 +0530

    Compression failed and bugs improved!

--- main.py
+++ main.py
@@ -26,22 +26,9 @@ def merger(plist, new_file):
         merger.append(pdf)
     merger.write(f"{new_file}.pdf")
 
-def compressed(file):
-    reader = PyPDF2.PdfReader(f"{file}.pdf")
-    writer = PyPDF2.PdfWriter()
-
-    for page in reader.pages:
-        page.compress_content_streams()
-        writer.add_page(page)
-
-    writer.add_metadata(reader.metadata)
-    with open(f"{file}_compress.pdf", "wb") as f:
-        writer.write(f)
 
 ins =  sys.argv[1:]
 root_directory = ins[0]
 super_file = ins[1]
 files = collector(root_directory)
-merger(files, super_file)
-compressed(super_file)
 print("Merged successfully...!")
