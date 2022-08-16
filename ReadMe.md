b4bf6cdc5cf711bb8bd86a79a0a325e79b77349d
Abhishek <abhishekmalhotra1292@gmail.com>
2022-08-16 22:03:51 +0530

    Merging pdf files in bulk successfull but supr_pdf is heavy genrated

---
+++ ReadMe.md
@@ -0,0 +1,20 @@
+<style>
+    .where {
+        text-decoration: underline;
+        font-family: ms-serif;
+    }
+</style>
+
+# Merger
+---
+
+This package come in action when pdfs are required to merge in a bulk.  
+
+# Usage 
+after cloning the repo enter the repo and run the main.py with the following arguments in command line.
+```
+python main.py root_directory new_file
+```
+
+<span class="where">root_directory:</span> The directory/Folder that contain all the files
+<span class="where">new_file:</span> Name of merged file
---
+++ main.py
@@ -0,0 +1,35 @@
+import sys
+import os
+import PyPDF2
+
+
+def collector(dirr):
+    splitext = os.path.splitext
+    listdir = os.listdir
+    files = []
+    for x in listdir(dirr):
+        if splitext(x)[1]=="":
+            for i in listdir(f"{dirr}\{x}"):
+                if splitext(i)[1]=="":
+                    for j in listdir(f"{dirr}\{x}\{i}"):
+                        if splitext(j)[1]==".pdf":
+                            files.append(f"{dirr}\{x}\{i}\{j}")
+                elif splitext(i)[1]==".pdf":
+                    files.append(f"{dirr}\{x}\{i}")
+        elif splitext(x)==".pdf":
+            files.append(f"{dirr}\{x}")
+    return files
+
+def merger(plist, new_file):
+    merger = PyPDF2.PdfFileMerger()
+    for pdf in plist:
+        merger.append(pdf)
+    merger.write(f"{new_file}.pdf")
+
+
+ins =  sys.argv[1:]
+root_directory = ins[0]
+super_file = ins[1]
+files = collector(root_directory)
+merger(files, super_file)
+print("Merged successfully...!")
