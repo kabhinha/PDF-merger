0e6cdd998b7389eccf72a8cab3a7f92de46a0df1
Abhishek <abhishekmalhotra1292@gmail.com>
2022-08-16 22:10:35 +0530

    Bugs improved and tred to compress super pdf

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def collector(dirr):
                             files.append(f"{dirr}\{x}\{i}\{j}")
                 elif splitext(i)[1]==".pdf":
                     files.append(f"{dirr}\{x}\{i}")
-        elif splitext(x)==".pdf":
+        elif splitext(x)[1]==".pdf":
             files.append(f"{dirr}\{x}")
     return files
 
@@ -26,10 +26,21 @@ def merger(plist, new_file):
         merger.append(pdf)
     merger.write(f"{new_file}.pdf")
 
+def compressed(file):
+    reader = PyPDF2.PdfReader(f"{file}.pdf")
+    writer = PyPDF2.PdfWriter()
+
+    for page in reader.pages:
+        page.compress_content_streams()  # This is CPU intensive!
+        writer.add_page(page)
+
+    with open(f"{file}_compress.pdf", "wb") as f:
+        writer.write(f)
 
 ins =  sys.argv[1:]
 root_directory = ins[0]
 super_file = ins[1]
 files = collector(root_directory)
 merger(files, super_file)
+compressed(super_file)
 print("Merged successfully...!")
